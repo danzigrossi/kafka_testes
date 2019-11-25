7a6ab182692d093314f64a9aec511a6939823186
Danilo Rafael Franco de Camargo Zigr <dzigrossi@lnxcit021752.cit>
2019-11-25 08:20:20 -0300

    init

---
+++ src/main/java/com/example/demo/Consumer.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class Consumer {
+
+    private final Logger logger = LoggerFactory.getLogger(Producer.class);
+
+    @KafkaListener(topics = "<topic1>", groupId = "<consumer1>")
+    public void consumeDetail(String message) throws IOException {
+        logger.info(String.format("#### -> MESSAGE1 -> %s", message));
+    }
+
+    @KafkaListener(topics = "<topic2>", groupId = "<consumer2>")
+    public void consumeErrors(String message) throws IOException {
+        logger.info(String.format("==== -> MESSAGE2 -> %s", message));
+    }
+}
